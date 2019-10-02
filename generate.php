8c49608bb43d831cfa7bf9bf033c3a9e53baabee
Martin Ekelund <martin@kidkie.se>
2019-10-02 18:48:43 +0200

    Fixed some vars

--- generate.php
+++ generate.php
@@ -1,8 +1,13 @@
 <?php
 
-// Basic shapes in PHP GD
+// --------------------------------------------
+// Basic shapes in PHP GD, inspiration
+// --------------------------------------------
 // http://www.alphadevx.com/a/56-Basic-Shapes-in-PHP-GD
 
+// --------------------------------------------
+// Basic vars
+// --------------------------------------------
 $size = 300;
 
 $width = 400;
@@ -30,69 +35,81 @@ $shapecolor = imagecolorallocate($image, rand(0,255), rand(0,255), rand(0,255));
 $halfbfcolor = imagecolorallocate($image, rand(0,255), rand(0,255), rand(0,255));
 
 // --------------------------------------------
-// Rect
+// Rect that covers half size
 // --------------------------------------------
 ImageFilledRectangle($image, 0, $ypos, $width, $height, $halfbfcolor);
 
 // Diamond
 function drawDiamond($x, $y, $width, $color) {
-    // access the global image reference (the one outside this function)
+	// --------------------------------------------
+    // Access the global image reference 
+    // (the one outside this function)
+    // --------------------------------------------
     global $image;
- 
-    // here we work out the four points of the diamond
-    $p1_x = $x;
-    $p1_y = $y+($width/2);
- 
-    $p2_x = $x+($width/2);
-    $p2_y = $y;
- 
-    $p3_x = $x+$width;
-    $p3_y = $y+($width/2);
- 
-    $p4_x = $x+($width/2);
-    $p4_y = $y+$width;
- 
-    // now create an array of points to store these four points
-    $points = array($p1_x, $p1_y, $p2_x, $p2_y, $p3_x, $p3_y, $p4_x, $p4_y);
- 
-    // the number of vertices for our polygon (four as it is a diamond
-    $num_of_points = 4;
 	
-	ImageFilledPolygon($image, $points, $num_of_points, $color);
+	// --------------------------------------------
+    // Here we work out the four points of the diamond
+    // --------------------------------------------
+    $p1_x = $x;
+    $p1_y = $y + ($width/2);
+	
+    $p2_x = $x + ($width/2);
+    $p2_y = $y;
+	
+    $p3_x = $x + $width;
+    $p3_y = $y + ($width/2);
+	
+    $p4_x = $x + ($width/2);
+    $p4_y = $y + $width;
+	
+	// --------------------------------------------
+    // Now create an array of points to store 
+    // these four points
+    // --------------------------------------------
+    $points = array($p1_x, $p1_y, $p2_x, $p2_y, $p3_x, $p3_y, $p4_x, $p4_y);
+	
+	ImageFilledPolygon($image, $points, 4, $color);
 }
 
 // --------------------------------------------
 // Random shape
 // --------------------------------------------
-$randomNum = rand(1,5);
+$random_number = rand(1,5);
 
-if($randomNum == 1) {
+if($random_number == 1) {
+	
 	ImageFilledEllipse($image, $xpos, $ypos, $size, $size, $shapecolor);
-} elseif($randomNum == 2) {
-	$xpos = $xpos - $size / 2;
-	$ypos = $ypos - $size / 2;
+	
+} elseif($random_number == 2) {
+	
+	$xpos = $xpos - ($size/2);
+	$ypos = $ypos - ($size/2);
 	drawDiamond($xpos, $ypos, $size, $shapecolor);
-} elseif($randomNum == 3) {
-	$xpos = $xpos - $size / 2;
-	$ypos = $ypos - $size / 2;
-	ImageFilledRectangle($image, ($width / 4), ($height / 4), $size, $size, $shapecolor);
-} elseif($randomNum == 4) {
+	
+} elseif($random_number == 3) {
+	
+	$xpos = $width/4;
+	$ypos = $height/4;
+	ImageFilledRectangle($image, $xpos, $ypos, $size, $size, $shapecolor);
+	
+} elseif($random_number == 4) {
+	
 	$points = array(
 		0,  0,  // Point 1 (x, y)
 		($width/2), ($height-100), // Point 2 (x, y)
 		$width,  0,  // Point 3 (x, y)
-		240, 0,  // Point 4 (x, y)
 	);
-	ImageFilledPolygon($image, $points, 4, $shapecolor);
+	ImageFilledPolygon($image, $points, 3, $shapecolor);
+	
 } else {
-	$num_of_points = 4;
+	
 	$points = array(
-		70,  ($height-70),  // Point 1 (x, y)
-		($height/2), ($height-70), // Point 2 (x, y)
-		($width-70), ($height-70),  // Point 3 (x, y)
-		($width/2), 70,  // Point 4 (x, y)
+		40,  ($height-70),  // Point 1 (x, y)
+		($width-40), ($height-70), // Point 2 (x, y)
+		($width/2), 70,  // Point 3 (x, y)
 	);
-	ImageFilledPolygon($image, $points, $num_of_points, $shapecolor);
+	ImageFilledPolygon($image, $points, 3, $shapecolor);
+	
 }
 
 // --------------------------------------------
